f4a1beb003f384cbb90cd792f7472249867804f7
Constantine <constantine@MacBook-Pro-Constantine.local>
2024-08-12 18:32:27 +0500

    feat(utils): Исправил методы по рекурсивному преобразованию ключей словаря из camelCase  в snake_case в конвертере операций

--- src/utils/models_converters.py
+++ src/utils/models_converters.py
@@ -14,30 +14,45 @@ class OperationConverter:
 
     def __init__(self, operation: dict[str, Any]) -> None:
         self._model: Optional[Operation] = None
-        self.operation = operation
+        self._camel_operation = operation
+        self._snake_operation = self._camel_operation
 
     @property
     def model(self) -> Operation:
+        """Получение python объекта из json-объекта операции"""
+
         if self._model is None:
-            operation = self._from_camel_to_snake(self.operation)
+            operation = self._from_camel_to_snake(self._camel_operation)
             self._model = self.MODEL(**operation)
         return self._model
 
-    def _from_camel_to_snake(self, dict_: dict) -> dict[str, Any]:
+    def _from_camel_to_snake(
+        self, dict_: dict, base_snake_dict: Optional[dict[str, Any]] = None,
+    ) -> dict[str, Any]:
         """
         Рекурсивное изменение ключей словаря из camelCase в snake_case
         """
+        snake_dict = base_snake_dict or self._camel_operation
 
-        operation_copy = deepcopy(dict_)
-        for key, value in operation_copy.items():
+        for key, value in dict_.items():
             if isinstance(value, dict):
-                self._from_camel_to_snake(value)
-            else:
-                if intersection := (set(key) & set(ascii_uppercase)):
-                    for char in key:
-                        if char in intersection:
-                            key = key.replace(char, f"_{char.lower()}")
-                        self.operation.update({key: value})
-                if key == "from":
-                    self.operation["from_"] = self.operation.pop("from")
-        return self.operation
+                self._from_camel_to_snake(value, snake_dict[key])
+        new_keys = [
+            self.convert_string_to_snake_case(key) for key in snake_dict
+        ]
+        old_keys = deepcopy(list(snake_dict.keys()))
+        for new_key, old_key in zip(new_keys, old_keys):
+            if new_key == old_key == "from":
+                new_key = "from_"
+            snake_dict[new_key] = snake_dict.pop(old_key)
+
+        return self._camel_operation
+
+    @staticmethod
+    def convert_string_to_snake_case(string: str) -> Optional[str]:
+        snake_string: Optional[str] = None
+        if intersection := (set(string) & set(ascii_uppercase)):
+            snake_string = ""
+            for char in intersection:
+                snake_string += string.replace(char, f"_{char.lower()}")
+        return snake_string if snake_string else string
